c22a1464a7b30fb3d3cd0c4adbfff06c97418e7b
Takudzwa <takudzwamaseva020@gmail.com>
2023-08-05 22:10:51 +0200

    Configure mongo db connection

--- Server/index.js
+++ Server/index.js
@@ -2,9 +2,24 @@ const express = require("express");
 const app = express();
 
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 app.use(express.json());
 app.use(cors());
+
+const dbURL = process.env.DB_URL || "mongodb://127.0.0.1:27017/Signage";
+
+mongoose.connect(dbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log("Mongo Connection is open");
+  })
+  .catch((err) => {
+    console.log("Error connecting to Mongo");
+    console.log(err);
+  });
 
 const port = process.env.PORT || 3000;
 
