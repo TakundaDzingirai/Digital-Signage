b5206834557a7bbe940b0ce9b2ad3b2a665b83f4
Takunda <dzingietakue@gmail.com>
2023-09-05 10:39:21 +0200

    Jason updated/routing and screenDetail.jsx for specific screenContent created

---
+++ Client/src/ScreenDetail.jsx
@@ -0,0 +1,36 @@
+import { useParams } from 'react-router-dom';
+import { useEffect } from "react";
+import { useState } from "react";
+import Axios from "axios";
+export default function ScreenDetail({ listOfUsers,setListOfUsers }) {
+  const { id } = useParams();
+
+  // Find the screen with the matching ID in listOfUsers
+  const [screenData, setScreenData] = useState({});
+  useEffect(() => {
+    Axios.get(`http://localhost:3000/screens/${id}`)
+      .then((response) => {
+        setScreenData(response.data);
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+//   console.log(id)
+//   console.log(screenData);
+//   const screen = listOfUsers.find((s) => s._id === parseInt(id));
+
+//   if (!screen) {
+//     return <div>Screen not found</div>;
+//   }
+
+  return (
+    <div>
+      <h1>{screenData.screenName} </h1>
+      <h3>This screen was created @ {screenData.createdAt}</h3>
+      {<p>{screenData.department}</p>}
+      {/* Display other screen details here */}
+    </div>
+  );
+}
