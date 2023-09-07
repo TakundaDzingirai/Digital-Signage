58012153fc4148bfed840e8be02997962e34bfdc
Takudzwa <takudzwamaseva020@gmail.com>
2023-09-07 17:52:14 +0200

    Update the user model to use passport.js

--- Server/models/User.js
+++ Server/models/User.js
@@ -1,26 +1,41 @@
 const mongoose = require("mongoose");
-const {Schema} = mongoose;
+const passportLocalMongoose = require("passport-local-mongoose");
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
+  firstame: {
+    type: String,
+    required: [true, "First name must be supplied"],
+  },
 
-    username: {
-        type: String, 
-        required: [true, "username must be supplied"]
-    },
+  lastname: {
+    type: String,
+    required: [true, "Last name must be supplied"],
+  },
 
-    email: {
-        type: String,
-        required: [true, "Email address must be supplied"],
-        unique: true
-    },
+  department: {
+    type: String,
+    required: [true, "Department must be supplied"],
+  },
 
-    role: {
-        type: String,
-        enum: ["admin", "user"],
-        default: "user"
-    },
+  username: {
+    type: String,
+    required: [true, "username must be supplied"],
+  },
 
+  email: {
+    type: String,
+    required: [true, "Email address must be supplied"],
+    unique: true,
+  },
 
-})
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+});
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
