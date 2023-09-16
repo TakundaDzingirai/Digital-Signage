a5c2d2ea486eba799085f3770886c526e5b4dd58
Takudzwa <takudzwamaseva020@gmail.com>
2023-09-16 21:58:04 +0200

    Refactor to use screen Controller

---
+++ Server/controllers/screenController.js
@@ -0,0 +1,65 @@
+const Screen = require("../models/Screen");
+
+class screenController {
+  // Used to get all the screen
+  static async getAllScreens(req, res) {
+    let screens;
+    // Check if the authenticated user's role is "admin"
+    if (req.user.role === "admin") {
+      // If the user is an admin, retrieve all screens in the database and sort them alphabetically by department
+      screens = await Screen.find({}).sort({ department: 1 });
+    } else {
+      // If the user is not an admin, retrieve screens belonging to the user's department
+      screens = await Screen.find({ department: req.user.department });
+    }
+    res.json(screens);
+  }
+
+  // Used to create a new screen
+  static async createNewScreen(req, res) {
+    // Extract data for creating a new screen from the request body
+    const { screenName, department } = req.body;
+    // Set createdBy to be the authenticated user
+    const createdBy = req.user.id;
+    // Create a new screen objcet with supplied data
+    const screen = new Screen({ screenName, department, createdBy });
+    // save screen to database
+    await screen.save();
+    res.json("Screen created successfully!");
+  }
+
+  // Used to get detailed screen information
+  static async getDetailedInfo(req, res) {
+    // Find screen by id from the db
+    const screen = await Screen.findById(req.params.id);
+    if (!screen) {
+      return res.status(404).json({ Error: "Screen not found" });
+    }
+    res.json(screen);
+  }
+
+  // Used to delete a Screen
+  static async deleteScreen(req, res) {
+    // Find screen by id and delete
+    const screen = await Screen.findByIdAndDelete(req.params.id);
+    if (!screen) {
+      return res.status(400).json({ Error: "Screen not found" });
+    }
+    res.json("Screen deleted successfully");
+  }
+
+  // Used to update screen details
+  static async updateSceenDetails(req, res) {
+    // Find the screen by its ID and update it with the data in req.body
+    const screen = await Screen.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!screen) {
+      return res.status(400).json({ Error: "Screen not found" });
+    }
+    await screen.save();
+    res.json(screen);
+  }
+}
+
+module.exports = screenController;
--- Server/routes/screenRoutes.js
+++ Server/routes/screenRoutes.js
@@ -3,90 +3,33 @@ if (process.env.NODE_ENV !== "production") {
 }
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const Screen = require("../models/Screen");
 const { authenticateJwt } = require("../middleware/auth");
 const catchAsync = require("../utilities/catchAsync");
 const { validateScreen } = require("../middleware/validation");
+const screenController = require("../controllers/screenController");
 
 // This route will be used to display all the active screens
-router.get(
-  "/",
-  authenticateJwt, // Middleware for JWT authentication
-  catchAsync(async (req, res) => {
-    let screens;
-
-    // Check if the authenticated user's role is "admin"
-    if (req.user.role === "admin") {
-      // If the user is an admin, retrieve all screens in the database and sort them alphabetically by department
-      screens = await Screen.find({}).sort({ department: 1 });
-    } else {
-      // If the user is not an admin, retrieve screens belonging to the user's department
-      screens = await Screen.find({ department: req.user.department });
-    }
-    console.log("Logged in user", req.user);
-    res.json(screens);
-  })
-);
+router.get("/", authenticateJwt, catchAsync(screenController.getAllScreens));
 
 // This route will be used to create a new screen
 router.post(
   "/",
   authenticateJwt,
   validateScreen,
-  catchAsync(async (req, res) => {
-    // Extract data for creating a new screen from the request body
-    const { screenName, department } = req.body;
-    // Set createdBy to be the authenticated user
-    const createdBy = req.user.id;
-    // Create a new screen objcet with supplied data
-    const screen = new Screen({ screenName, department, createdBy });
-    // save screen to database
-    await screen.save();
-    res.json("Screen created successfully!");
-  })
+  catchAsync(screenController.createNewScreen)
 );
 
 // This route will be used to view detailed information about a screen
-router.get(
-  "/:id",
-  catchAsync(async (req, res) => {
-    // Find screen by id from the db
-    const screen = await Screen.findById(req.params.id);
-    if (!screen) {
-      return res.status(404).json({ Error: "Screen not found!!!" });
-    }
-    res.json(screen);
-  })
-);
+router.get("/:id", catchAsync(screenController.getDetailedInfo));
 
 // This route will be used to delete a screen
-router.delete(
-  "/:id",
-  catchAsync(async (req, res) => {
-    // Find screen by id and delete
-    const screen = await Screen.findByIdAndDelete(req.params.id);
-    if (!screen) {
-      return res.status(400).json({ Error: "Screen not found@@@" });
-    }
-    res.json("Screen deleted successfully");
-  })
-);
+router.delete("/:id", catchAsync(screenController.deleteScreen));
 
 // This route will be used to update/edit a screen
 router.put(
   "/:id",
   validateScreen,
-  catchAsync(async (req, res) => {
-    // Find the screen by its ID and update it with the data in req.body
-    const screen = await Screen.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    if (!screen) {
-      return res.status(400).json({ Error: "Screen not found$$$" });
-    }
-    await screen.save();
-    res.json(screen);
-  })
+  catchAsync(screenController.updateSceenDetails)
 );
 
 module.exports = router;
