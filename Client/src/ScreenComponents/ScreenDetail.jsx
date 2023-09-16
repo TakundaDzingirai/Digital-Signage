4332f99da2ee0465fd8d702cc52a50b483e2a0db
Takunda <dzingietakue@gmail.com>
2023-09-16 23:51:35 +0200

    circular progress bar at right place

--- Client/src/ScreenComponents/ScreenDetail.jsx
+++ Client/src/ScreenComponents/ScreenDetail.jsx
@@ -1,44 +1,42 @@
 import { useNavigate } from 'react-router-dom';
-import { useParams } from 'react-router-dom'; // Import useParams and useHistory
+import { useParams } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import Axios from "axios";
 import "./ScreenDetail.css"
-import ScreenContentForm from "../React-forms/ScreenContentForm";
 import { TextField, Button } from "@mui/material";
 import Slides from './Slides';
 import ScreenPanel from './ScreenPanel';
+import CircularIndeterminate from '../CircularIndeterminate';
 
 export default function ScreenDetail() {
   const { id } = useParams();
-  const history = useNavigate(); // Create a history object
-
-  // Find the screen with the matching ID in listOfUsers
+  const history = useNavigate();
   const [screenData, setScreenData] = useState({});
+  const [loading, setLoading] = useState(true); // Initially, set loading to true
+
   useEffect(() => {
     Axios.get(`http://localhost:3000/screens/${id}`)
       .then((response) => {
         setScreenData(response.data);
-        console.log(response.data)
+        setLoading(false); // Set loading to false when data is retrieved
       })
       .catch((error) => {
         console.error(error);
+        setLoading(false); // Make sure to set loading to false on error as well
       });
-  }, []);
+  }, [id]);
 
   const handleButtonClick = () => {
-    // Navigate to the CreateContentForm component
     history(`/screens/content/${id}`);
   };
 
   return (
-    <div>
+    <div style={{ position: "relative" }}>
       <ScreenPanel />
+      {loading && (<CircularIndeterminate />)} {/* Show loader while loading */}
       <h1>{screenData.screenName} </h1>
-
       <Slides key={id} />
-
       <Button
-
         onClick={handleButtonClick}
         variant="outlined"
         color="secondary"
