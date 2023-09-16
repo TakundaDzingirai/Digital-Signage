2c78615fd4ad164d7b93030b1a50c29c5da0c164
Takudzwa <takudzwamaseva020@gmail.com>
2023-09-16 15:53:18 +0200

    update the screen routes and add catchAsync

--- Server/routes/screenRoutes.js
+++ Server/routes/screenRoutes.js
@@ -1,71 +1,80 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const Screen = require("../models/Screen");
 const { authenticateJwt } = require("../middleware/auth");
+const catchAsync = require("../utilities/catchAsync");
 
 // This route will be used to display all the active screens
-router.get("/", authenticateJwt, async (req, res) => {
-  try {
-    console.log("THE LOGGED IN USER....... ", req.user);
-    // Retrieve all screens from the database
-    const screens = await Screen.find({ department: req.user.department });
-    console.log("THE LOGGED IN USER....... ", req.user);
+router.get(
+  "/",
+  authenticateJwt, // Middleware for JWT authentication
+  catchAsync(async (req, res) => {
+    let screens;
+
+    // Check if the authenticated user's role is "admin"
+    if (req.user.role === "admin") {
+      // If the user is an admin, retrieve all screens in the database and sort them alphabetically by department
+      screens = await Screen.find({}).sort({ department: 1 });
+    } else {
+      // If the user is not an admin, retrieve screens belonging to the user's department
+      screens = await Screen.find({ department: req.user.department });
+    }
+    console.log("Logged in user", req.user);
     res.json(screens);
-  } catch (err) {
-    console.log(err.message);
-    res.status(400).json({ "Error ": err.message });
-  }
-});
+  })
+);
 
 // This route will be used to create a new screen
-router.post("/", authenticateJwt, async (req, res) => {
-  try {
-    console.log(req.body);
+router.post(
+  "/",
+  authenticateJwt,
+  catchAsync(async (req, res) => {
+    // Extract data for creating a new screen from the request body
     const { screenName, department } = req.body;
+    // Set createdBy to be the authenticated user
     const createdBy = req.user.id;
+    // Create a new screen objcet with supplied data
     const screen = new Screen({ screenName, department, createdBy });
-    console.log("THE SCREEN: ", screen);
+    // save screen to database
     await screen.save();
     res.json("Screen created successfully!");
-  } catch (err) {
-    console.error("Error creating screen:", err.message);
-    res.status(400).json({ "Error ": err.message });
-  }
-});
+  })
+);
 
 // This route will be used to view detailed information about a screen
-router.get("/:id", async (req, res) => {
-  try {
+router.get(
+  "/:id",
+  catchAsync(async (req, res) => {
+    // Find screen by id from the db
     const screen = await Screen.findById(req.params.id);
     if (!screen) {
       return res.status(404).json({ Error: "Screen not found!!!" });
     }
     res.json(screen);
-  } catch (err) {
-    res.status(400).json({ "Error ": err.message });
-  }
-});
+  })
+);
 
 // This route will be used to delete a screen
-router.delete("/:id", async (req, res) => {
-  try {
+router.delete(
+  "/:id",
+  catchAsync(async (req, res) => {
+    // Find screen by id and delete
     const screen = await Screen.findByIdAndDelete(req.params.id);
     if (!screen) {
       return res.status(400).json({ Error: "Screen not found@@@" });
     }
     res.json("Screen deleted successfully");
-  } catch (err) {
-    res.status(400).json({ "Error ": err.message });
-  }
-});
+  })
+);
 
 // This route will be used to update/edit a screen
-router.put("/:id", async (req, res) => {
-  try {
+router.put(
+  "/:id",
+  catchAsync(async (req, res) => {
+    // Find the screen by its ID and update it with the data in req.body
     const screen = await Screen.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
@@ -74,9 +83,7 @@ router.put("/:id", async (req, res) => {
     }
     await screen.save();
     res.json(screen);
-  } catch (err) {
-    res.status(400).json({ Error: err.message });
-  }
-});
+  })
+);
 
 module.exports = router;
