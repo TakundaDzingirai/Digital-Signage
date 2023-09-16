fb85d3b63e68e4ce651820d15f8a50fed9388db2
Takudzwa <takudzwamaseva020@gmail.com>
2023-09-16 18:19:34 +0200

    Refactor the schema validation middlwares

--- Server/middleware/validation.js
+++ Server/middleware/validation.js
@@ -7,39 +7,9 @@ const {
 } = require("../Schemas/Schema");
 const Joi = require("joi");
 
-const validateScreen = (req, res, next) => {
-  // Validate the request body against the screenSchema
-  const { error } = screenSchema.validate(req.body);
-
-  if (error) {
-    // If validation fails, construct an ErrorResponse and throw it
-    const errorMsg = error.details.map((el) => el.message).join(",");
-    const validationError = new ErrorResponse(errorMsg, 400);
-    // Pass the error to the error handling middleware
-    next(validationError);
-  } else {
-    next();
-  }
-};
-
-const validateContent = (req, res, next) => {
-  // Validate the request body against the contentSchema
-  const { error } = contentSchema.validate(req.body);
-
-  if (error) {
-    // If validation fails, construct an ErrorResponse and throw it
-    const errorMsg = error.details.map((el) => el.message).join(",");
-    const validationError = new ErrorResponse(errorMsg, 400);
-    // Pass the error to the error handling middleware
-    next(validationError);
-  } else {
-    next();
-  }
-};
-
-const validateUser = (req, res, next) => {
-  // Validate the request body against the contentSchema
-  const { error } = userSchema.validate(req.body);
+const validateSchema = (schema) => (req, res, next) => {
+  // Validate the request body against the provided schema
+  const { error } = schema.validate(req.body);
 
   if (error) {
     // If validation fails, construct an ErrorResponse and throw it
@@ -53,7 +23,7 @@ const validateUser = (req, res, next) => {
 };
 
 module.exports = {
-  validateScreen,
-  validateContent,
-  validateUser,
+  validateScreen: validateSchema(screenSchema),
+  validateContent: validateSchema(contentSchema),
+  validateUser: validateSchema(userSchema),
 };
