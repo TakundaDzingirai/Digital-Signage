03b3e6d77bbe4e29fd8b2d8e995293a71ddfd6ac
Takudzwa <takudzwamaseva020@gmail.com>
2023-09-16 22:24:17 +0200

    Refactor to use contentController

---
+++ Server/controllers/contentController.js
@@ -0,0 +1,89 @@
+const Content = require("../models/Content");
+const Screen = require("../models/Screen");
+
+class contentController {
+  static async addContentToScreen(req, res) {
+    // Extract data from the request body
+    const { slideTitle, post, imageUrl } = req.body;
+    // Get the screen ID from the URL parameters
+    const screenId = req.params.screenId;
+
+    // const createdBy = req.user.id;
+
+    // Create an object with the content data
+    console.log(req.user);
+    const contentData = {
+      slideTitle,
+      post,
+      imageUrl,
+      screen: screenId,
+      // createdBy,
+    };
+    // Create a new Content instance with the content data
+    const content = new Content(contentData);
+    // Save the new content to the database
+    const savedContent = await content.save();
+    // Update the Screen document to include the new content's ID
+    await Screen.findByIdAndUpdate(screenId, {
+      $push: { content: savedContent._id },
+    });
+
+    res.json(savedContent);
+  }
+
+  static async deleteContent(req, res) {
+    const { contentId } = req.params;
+    // Find the content by its ID
+    const content = await Content.findById(contentId);
+    if (!content) {
+      res.status(400).json({ Error: "Content not found" });
+    }
+    // Get the screen ID associated with the content
+    const screenId = content.screen;
+    // Remove the content ID from the associated screen
+    await Screen.findByIdAndUpdate(screenId, {
+      $pull: { content: contentId },
+    });
+    res.json({ message: "Content deleted successfully" });
+  }
+
+  static async editContent(req, res) {
+    const { contentId } = req.params;
+    // Extract content data from the re.body
+    const { slideTitle, post, imageUrl } = req.body;
+    // Find the content by its ID
+    const content = await Content.findById(contentId);
+    if (!content) {
+      return res.status(404).json({ error: "Content not found" });
+    }
+    // Update the content's properties with the provided data
+    content.slideTitle = slideTitle;
+    content.post = post;
+    content.imageUrl = imageUrl;
+    // Save the updated content
+    const updatedContent = await content.save();
+    res.json(updatedContent);
+  }
+
+  static async showAScreenScontent(req, res) {
+    const { screenId } = req.params;
+    // Find the screen by its ID and populate its content field
+    const screen = await Screen.findById(screenId).populate("content");
+    if (!screen) {
+      return res.status(404).json({ Error: "Screen not found***" });
+    }
+    res.json(screen.content);
+  }
+
+  static async showDetailedContent(req, res) {
+    const { contentId } = req.params;
+    // Find the content by its ID
+    const content = await Content.findById(contentId);
+    if (!content) {
+      res.status(404).json({ error: "Content not found" });
+    }
+    res.json(content);
+  }
+}
+
+module.exports = contentController;
--- Server/routes/contentRoutes.js
+++ Server/routes/contentRoutes.js
@@ -2,8 +2,6 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const express = require("express");
-const Content = require("../models/Content");
-const Screen = require("../models/Screen");
 const router = express.Router({ mergeParams: true });
 const { storage } = require("../cloudinary/index.js");
 const multer = require("multer");
@@ -17,109 +15,32 @@ const upload = multer({
 
 const catchAsync = require("../utilities/catchAsync");
 const { validateContent } = require("../middleware/validation");
+const contentController = require("../controllers/contentController.js");
 
 // This route will be used to add content to a screen
 router.post(
   "/:screenId",
   validateContent,
-  catchAsync(async (req, res) => {
-    // Extract data from the request body
-    const { slideTitle, post, imageUrl } = req.body;
-    // Get the screen ID from the URL parameters
-    const screenId = req.params.screenId;
-    // Get the ID of the authenticated user
-    // const createdBy = req.user.id;
-    // Create an object with the content data
-    console.log(req.user)
-    const contentData = {
-      slideTitle,
-      post,
-      imageUrl,
-      screen: screenId,
-      // createdBy,
-    };
-    // Create a new Content instance with the content data
-    const content = new Content(contentData);
-    // Save the new content to the database
-    const savedContent = await content.save();
-    // Update the Screen document to include the new content's ID
-    await Screen.findByIdAndUpdate(screenId, {
-      $push: { content: savedContent._id },
-    });
-
-    res.json(savedContent);
-  })
+  catchAsync(contentController.addContentToScreen)
 );
 
 // This route will be used to delete content from a screen
-router.delete(
-  "/:contentId",
-  catchAsync(async (req, res) => {
-    const { contentId } = req.params;
-    // Find the content by its ID
-    const content = await Content.findById(contentId);
-    if (!content) {
-      res.status(400).json({ Error: "Content not found" });
-    }
-    // Get the screen ID associated with the content
-    const screenId = content.screen;
-    // Remove the content ID from the associated screen
-    await Screen.findByIdAndUpdate(screenId, {
-      $pull: { content: contentId },
-    });
-    res.json({ message: "Content deleted successfully" });
-  })
-);
+router.delete("/:contentId", catchAsync(contentController.deleteContent));
 
 // This route will be used to edit individual screen content
 router.put(
   "/edit/:contentId",
   validateContent,
-  catchAsync(async (req, res) => {
-    const { contentId } = req.params;
-    // Extract content data from the re.body
-    const { slideTitle, post, imageUrl } = req.body;
-    // Find the content by its ID
-    const content = await Content.findById(contentId);
-    if (!content) {
-      return res.status(404).json({ error: "Content not found" });
-    }
-    // Update the content's properties with the provided data
-    content.slideTitle = slideTitle;
-    content.post = post;
-    content.imageUrl = imageUrl;
-    // Save the updated content
-    const updatedContent = await content.save();
-    res.json(updatedContent);
-  })
+  catchAsync(contentController.editContent)
 );
 
 // This route will be used to show all the content on a  specific screen
-router.get(
-  "/:screenId",
-  catchAsync(async (req, res) => {
-    const { screenId } = req.params;
-    // Find the screen by its ID and populate its content field
-    const screen = await Screen.findById(screenId).populate("content");
-    if (!screen) {
-      return res.status(404).json({ Error: "Screen not found***" });
-    }
-    res.json(screen.content);
-  })
-);
+router.get("/:screenId", catchAsync(contentController.showAScreenScontent));
 
 // This route will be used to view detailed information about a s specific content item
 router.get(
   "/more/:contentId",
-  catchAsync(async (req, res) => {
-    const { contentId } = req.params;
-    // Find the content by its ID
-    const content = await Content.findById(contentId);
-    if (!content) {
-      res.status(404).json({ error: "Content not found" });
-    }
-    res.json(content);
-  })
+  catchAsync(contentController.showDetailedContent)
 );
 
 // Consider route for reordering the contents on a screen(Drag and drop functionality)
