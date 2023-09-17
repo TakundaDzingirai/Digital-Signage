ad126818a329120469f3a94eb314858a61404faf
Takudzwa <takudzwamaseva020@gmail.com>
2023-09-17 21:16:25 +0200

    Refactor the createScreenForm component

--- Client/src/React-forms/CreateScreenForm.jsx
+++ Client/src/React-forms/CreateScreenForm.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./FormPopup.css";
 import Button from "../Button_btn";
 import Axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function CreateScreenForm({
   listOfScreen,
@@ -11,35 +13,38 @@ export default function CreateScreenForm({
 }) {
   const [screenName, setScreenName] = useState("");
   const [department, setDepartment] = useState("");
-  const [id, setId] = useState("");
-  const [found, setFound] = useState(false);
 
-  const createScreen = (e) => {
+  const createScreen = async (e) => {
     e.preventDefault();
     const token = localStorage.getItem("token");
 
-    if (!token) return;
-
+    if (!token) {
+      toast.error("You must be logged in to create a screen.");
+      return;
+    }
     const headers = {
       Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
     };
 
-    Axios.post(
-      "http://localhost:3000/screens",
-      {
-        screenName,
-        department,
-      },
-      { headers }
-    )
-      .then((response) => {
-        setListOfScreen([...listOfScreen, { screenName, department }]);
-        // window.location.reload();
-      })
-      .catch((error) => {
-        console.error("Error creating screen:", error);
-      });
+    try {
+      const response = await Axios.post(
+        "http://localhost:3000/screens",
+        {
+          screenName,
+          department,
+        },
+        { headers }
+      );
+      setListOfScreen([...listOfScreen, { screenName, department }]);
+      toast.success(response.data);
+    } catch (error) {
+      if (error.response) {
+        toast.error(error.response.data.error);
+      } else {
+        toast.error("An error occurred while creating the screen.");
+      }
+    }
 
     setScreenName("");
     setDepartment("");
