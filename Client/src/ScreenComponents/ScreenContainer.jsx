89b773a17bbe807163aa34906da7e8e3abb195e6
Takudzwa <takudzwamaseva020@gmail.com>
2023-09-18 14:57:13 +0200

    Create Footer component and style it

--- Client/src/ScreenComponents/ScreenContainer.jsx
+++ Client/src/ScreenComponents/ScreenContainer.jsx
@@ -1,34 +1,19 @@
 import Header from "../Header";
 import Screens from "./Screens";
 import Addbtn from "../Addbtn.jsx";
-import { TextField, Button } from "@mui/material";
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
-import DeleteIcon from '@mui/icons-material/Delete';
-
-import React, { useState, useEffect } from "react";
 import CreatescreenForm from "../React-forms/CreateScreenForm";
-
-import { useLocation } from "react-router-dom";
+import Footer from "../Footer";
 export default function ScreenContainer({
   showForm,
   onToggleForm,
   listOfScreen,
   setListOfScreen,
 }) {
-
-  const location = useLocation();
-  const { departmnt } = location.state || {};
-  console.log("i got the Department:", departmnt)
-  useEffect(() => {
-    // You can access departmnt here when it's updated
-    console.log("Department in useEffect:", departmnt);
-  }, [departmnt]);
-
   return (
     <div>
       <Header />
-      <Screens listOfScreen={listOfScreen} setListOfScreen={setListOfScreen} departmnt={departmnt} />
+      <h1>Screen Container Start</h1>
+      <Screens listOfScreen={listOfScreen} setListOfScreen={setListOfScreen} />
 
       {showForm && (
         <CreatescreenForm
@@ -38,10 +23,13 @@ export default function ScreenContainer({
           onToggleForm={onToggleForm}
         />
       )}
-      <Addbtn buttonName={"Add Screen"} showForm={showForm} onToggleForm={onToggleForm} />
-
-
-      <hr />
+      <Addbtn
+        buttonName={"Add Screen"}
+        showForm={showForm}
+        onToggleForm={onToggleForm}
+      />
+      <h1>Screen Container End</h1>
+      <Footer />
     </div>
   );
 }
