bb2d74726cb14dbd2517ebdc20c3b0cdb7184775
Takudzwa <takudzwamaseva020@gmail.com>
2023-09-19 02:14:18 +0200

    Add circular progress to the slides component

--- Client/src/ScreenComponents/Slides.jsx
+++ Client/src/ScreenComponents/Slides.jsx
@@ -2,17 +2,24 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Axios from "axios";
 import Slide from "./Slide";
-import { Button, Grid, Container, Box } from "@mui/material";
+import { Button, Grid, Container, Box, CircularProgress } from "@mui/material";
 
 export default function Slides() {
   const { id } = useParams();
   const [sldData, setsldData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await Axios.get(`http://localhost:3000/content/${id}`);
-      setsldData(response.data);
+      try {
+        const response = await Axios.get(`http://localhost:3000/content/${id}`);
+        setsldData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.log("Error fetching data", error);
+        setLoading(false);
+      }
     };
     fetchData();
   }, [id]);
@@ -24,20 +31,33 @@ export default function Slides() {
   return (
     <Container>
       <Box my={2}>
-        <Grid container spacing={2}>
-          {sldData.map((s) => (
-            <Grid item xs={12} sm={6} lg={3} key={s._id}>
-              <Slide s={s} screenId={id} />
-            </Grid>
-          ))}
-        </Grid>
+        {loading ? (
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="100vh"
+          >
+            <CircularProgress />
+          </Box>
+        ) : (
+          <Grid container spacing={2}>
+            {sldData.map((s) => (
+              <Grid item xs={12} sm={6} lg={3} key={s._id}>
+                <Slide s={s} screenId={id} />
+              </Grid>
+            ))}
+          </Grid>
+        )}
       </Box>
 
-      <Box textAlign="center">
-        <Button onClick={handlePreview} variant="outlined" color="primary">
-          Preview
-        </Button>
-      </Box>
+      {!loading && (
+        <Box textAlign="center">
+          <Button onClick={handlePreview} variant="outlined" color="primary">
+            Preview
+          </Button>
+        </Box>
+      )}
     </Container>
   );
 }
