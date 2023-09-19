1c6c57825bdeff8e2fbb481e9aaa043447718d58
Takunda <dzingietakue@gmail.com>
2023-09-19 21:44:26 +0200

    optimised the Server to handle upload files faster

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 const connectToDatabase = require("./database/db.js");
-const path = require('path'); // Node.js built-in module for file paths
-const busboy = require('busboy'); // Add busboy for streaming
-const fs = require('fs'); // Import the 'fs' module
+const path = require('path');
+const busboy = require('busboy');
+const fs = require('fs');
+const sharp = require('sharp'); // Import sharp for image processing
+const axios = require('axios'); // Import axios for streaming
 
 // Define the destination folder for uploaded files using disk storage
 const uploadDir = path.join(__dirname, 'uploads');
@@ -27,7 +30,10 @@ app.use("/content", require("./routes/contentRoutes")); // Define your routes se
 app.post('/upload', (req, res) => {
   try {
     const busboyInstance = new busboy({ headers: req.headers });
-    const fileWriteStream = fs.createWriteStream(path.join(uploadDir, 'uploadedFile')); // Set your desired file name
+
+    // Create a temporary file for the uploaded image
+    const tempFilePath = path.join(uploadDir, 'tempUploadedFile'); // Change the temporary file name if needed
+    const fileWriteStream = fs.createWriteStream(tempFilePath);
 
     busboyInstance.on('file', (fieldname, file, filename) => {
       // Pipe the file stream to the write stream
@@ -36,32 +42,45 @@ app.post('/upload', (req, res) => {
 
     busboyInstance.on('finish', () => {
       // File upload is complete
-      // You can now process or upload the file as needed
-      // For example, you can use the 'fs' module to read the file and perform actions on it
-      fs.readFile(path.join(uploadDir, 'uploadedFile'), (err, data) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).send('An error occurred while processing the file.');
-        }
 
-        // Process the file data here
-
-        // Now, you can upload the file to Cloudinary
-        cloudinary.uploader.upload(data, {
-          folder: 'DigiSign', // Set your desired folder
-          // Additional Cloudinary upload options here
-        }, (uploadError, result) => {
-          if (uploadError) {
-            console.error(uploadError);
-            return res.status(500).send('Error uploading to Cloudinary.');
+      // Process and compress the uploaded image using sharp
+      sharp(tempFilePath)
+        .resize({ width: 800 }) // Adjust dimensions as needed
+        .toBuffer(async (err, buffer) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).send('Error processing the image.');
           }
 
-          // You can access the Cloudinary URL in `result.secure_url`
-          const cloudinaryUrl = result.secure_url;
+          // Now, you can upload the compressed image to Cloudinary
+          try {
+            const cloudinaryResponse = await axios.post(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`, buffer, {
+              headers: {
+                'Content-Type': 'application/octet-stream',
+              },
+              params: {
+                folder: 'DigiSign', // Set your desired folder
+                // Additional Cloudinary upload options here
+              },
+            });
 
-          // Perform any additional processing or database storage as needed
-          res.status(200).json({ message: 'File uploaded successfully', cloudinaryUrl });
+            const cloudinaryUrl = cloudinaryResponse.data.secure_url;
+
+            // Perform any additional processing or database storage as needed
+
+            // Send a response
+            res.status(200).json({ message: 'Image uploaded and processed successfully', cloudinaryUrl });
+          } catch (uploadError) {
+            console.error(uploadError);
+            res.status(500).send('Error uploading to Cloudinary.');
+          }
         });
+
+      // Clean up the temporary file
+      fs.unlink(tempFilePath, (unlinkError) => {
+        if (unlinkError) {
+          console.error('Error deleting the temporary file:', unlinkError);
+        }
       });
     });
 
