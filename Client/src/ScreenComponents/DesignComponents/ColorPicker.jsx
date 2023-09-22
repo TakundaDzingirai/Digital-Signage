38cbe973d9a4fca7cedc300812c292dbc735fde3
Takunda <dzingietakue@gmail.com>
2023-09-22 14:14:20 +0200

    handle zIndex(which component should be on top of the other)

--- Client/src/ScreenComponents/DesignComponents/ColorPicker.jsx
+++ Client/src/ScreenComponents/DesignComponents/ColorPicker.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import { ChromePicker } from 'react-color';
 import TextField from '@mui/material/TextField';
+import Popover from '@mui/material/Popover';
 import {
     Divider,
-
 } from '@mui/material';
+
 export default function ColorPicker({ setTextColor, textColor, setBackgroundColor, backgroundColor }) {
     // Initial background color
-    const [showTextColorPicker, setShowTextColorPicker] = useState(false);
-    const [showBgColorPicker, setShowBgColorPicker] = useState(false);
+    const [anchorElTextColor, setAnchorElTextColor] = useState(null);
+    const [anchorElBgColor, setAnchorElBgColor] = useState(null);
 
     const handleTextColorChange = (color) => {
         setTextColor(color.hex);
@@ -18,58 +19,85 @@ export default function ColorPicker({ setTextColor, textColor, setBackgroundColo
         setBackgroundColor(color.hex);
     };
 
-    const toggleTextColorPicker = () => {
-        setShowTextColorPicker(!showTextColorPicker);
+    const openTextColorPicker = (event) => {
+        setAnchorElTextColor(event.currentTarget);
     };
 
-    const toggleBgColorPicker = () => {
-        setShowBgColorPicker(!showBgColorPicker);
+    const openBgColorPicker = (event) => {
+        setAnchorElBgColor(event.currentTarget);
+    };
+
+    const closeTextColorPicker = () => {
+        setAnchorElTextColor(null);
+    };
+
+    const closeBgColorPicker = () => {
+        setAnchorElBgColor(null);
     };
 
     return (
         <div style={{
-            display: "flex", height: "13vh", width: "100%", flexDirection: "column"
+            display: "flex", height: "13vh", width: "100%", flexDirection: "column", position: "relative"
         }}>
-            Slect Color
-            <div style={{ display: 'flex', justifyContent: "space-between", width: "100%" }}>
-
+            Select Color
+            <div style={{ display: 'flex', justifyContent: "space-between", width: "100%", position: "relative " }}>
                 <div style={{ marginTop: "1vh", height: "5vh", width: "100%" }}>
                     <TextField
                         label="Text Color"
                         variant="outlined"
                         value={textColor}
-                        onClick={toggleTextColorPicker}
+                        onClick={openTextColorPicker}
                         style={{ backgroundColor: textColor, height: "4vh" }}
                     />
-                    {showTextColorPicker && (
+                    <Popover
+                        open={Boolean(anchorElTextColor)}
+                        anchorEl={anchorElTextColor}
+                        onClose={closeTextColorPicker}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                    >
                         <ChromePicker
                             color={textColor}
                             onChange={handleTextColorChange}
                             disableAlpha={true}
-
                         />
-                    )}
+                    </Popover>
                 </div>
                 <div style={{ marginTop: "1vh", height: "5vh", width: "100%" }}>
                     <TextField
                         label="Background Color"
                         variant="outlined"
                         value={backgroundColor}
-                        onClick={toggleBgColorPicker}
+                        onClick={openBgColorPicker}
                         style={{ backgroundColor: backgroundColor, height: "4vh" }}
                     />
-
-                    {showBgColorPicker && (
+                    <Popover
+                        open={Boolean(anchorElBgColor)}
+                        anchorEl={anchorElBgColor}
+                        onClose={closeBgColorPicker}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                    >
                         <ChromePicker
                             color={backgroundColor}
                             onChange={handleBgColorChange}
                             disableAlpha={true}
                         />
-                    )}
+                    </Popover>
                 </div>
-
             </div>
-
         </div>
     );
 }
